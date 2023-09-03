45ff2655dbaf3a827688aa4fd31b0d34744d091b
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2023-09-04 01:35:42 +0300

    fixed a bug downvote

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("create/", views.PostCreateView.as_view(), name="create_post"),
     path("<int:pk>/upvote", views.upvote, name="upvote"),
-    path("<int:pk>/downvote", views.upvote, name="downvote"),
+    path("<int:pk>/downvote", views.downvote, name="downvote"),
 ]
