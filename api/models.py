f89304c1e3b6f188e00015a9a1722bcdf6c47df9
StankevichAngelina <79199054+StankevichAngelina@users.noreply.github.com>
2023-09-17 00:47:41 +0300

    tags

---
+++ api/models.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+from polls.models import Question
+from blog.models import Post
+
+__all__ = ['User', 'Post', 'Question']
--- examplesite/settings.py
+++ examplesite/settings.py
@@ -160,11 +160,3 @@ REST_FRAMEWORK = {
     ]
 }
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
-
